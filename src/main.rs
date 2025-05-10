43a8f13cde57aa5a5cf3dc2c1d7e8896dc88590d
WhenitStarted <69560016+WhenitStarted@users.noreply.github.com>
2025-05-10 16:02:40 -0700

    req user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+// use std::io;
+
+fn main() 
+{
+    println!("Guess the number");
+
+    println!("Input your number to guess");
+
+    let mut guess = String::new();
+
+    std::io::stdin().read_line(&mut guess).expect("failed to read line");
+
+    println!("You guessed: {}", guess);
 }
